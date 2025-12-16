92ce9646d3fcd0bc2f5699b37a7304b719c01c22
H-Manuel <manuhofer2003@gmail.com>
2025-12-16 16:25:30 +0100

    tested esp with platform io

--- Code/Roboterplattform/src/main.cpp
+++ Code/Roboterplattform/src/main.cpp
@@ -1,15 +1,8 @@
 #include <Arduino.h>
 
-// empty project
-
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
